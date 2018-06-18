0326ff692b90d02fbaf7ff8a352d1a1008ca3e2d
Dmitriy Paramoshkin <dmitriy.paramoshkin@gmail.com>
2018-06-18 12:48:58 +0200

    extract connection url for frequency generator to properties file

--- frequency-tracker/src/main/java/com/younicos/frequencytracker/job/FrequencyRequester.java
+++ frequency-tracker/src/main/java/com/younicos/frequencytracker/job/FrequencyRequester.java
@@ -4,6 +4,7 @@ package com.younicos.frequencytracker.job;
 import com.younicos.frequencytracker.event.FrequencyUpdateEvent;
 import com.younicos.frequencytracker.model.Frequency;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,14 @@ public class FrequencyRequester {
     @Autowired
     private ApplicationEventPublisher applicationEventPublisher;
 
+    @Value("${frequencySource.url}")
+    private String frequencySourceUrl;
+
     @Scheduled(fixedRate=100)
     public void readFrequencies(){
         RestTemplate restTemplate = new RestTemplate();
         try {
-            Frequency frequency = restTemplate.getForObject("http://127.0.0.1:8080/frequency", Frequency.class);
+            Frequency frequency = restTemplate.getForObject(frequencySourceUrl, Frequency.class);
             publishFrequencyUpdateEvent(frequency);
         } catch (Exception ex){
             publishFrequencyUpdateEvent(null);
